c9858c36a8f51461e5356fd5b6e1f1eeb3edf828
Roman Pikhotskiy <romapanda5@mail.ru>
2023-07-27 23:37:42 +0500

    first minus

--- project1.py
+++ project1.py
@@ -1,14 +1,23 @@
+def minus(number1, number2):
+    return number1 - number2
+
+
 def main ():
     print('Приветствуем в нашем калькуляторе')
     print('Выберите действие которое хотите совершить')
     print('Сложить два числа: +')
     print('Найти разность чисел: -')
-    choice = input()
+    choice = input(':')
+    number1 = float(input('Введите первое число: '))
+    number2 = float(input('Введите второе число: '))
     if choice == '+':
-        summa()
+        answer  = summa(number1, number2)
     elif choice == '-':
-        minus()
+        answer = minus(number1, number2)
     else :
         error()
+
+    print(number1 , choice, number2, '=', answer)
+    print('Ответ:', answer)
 
 main()
